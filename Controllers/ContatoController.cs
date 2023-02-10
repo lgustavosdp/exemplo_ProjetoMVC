049e8cb2c60e72cfc0efdd9c7729305c392757eb
lgustavosdp <lgustavosdp@yahoo.com.br>
2023-02-10 15:21:19 -0300

    ATUALIZANDO

---
+++ Controllers/ContatoController.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using PROJETOMVC.Context;
+
+namespace PROJETOMVC.Controllers
+{
+    public class ContatoController : Controller
+    {
+        private readonly AgendaContext _context;
+        public ContatoController (AgendaContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var contatos = _context.Contatos.ToList();
+            return View();
+        }
+
+    }
+}
---
+++ Models/Contato.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PROJETOMVC.Models
+{
+    public class Contato
+    {
+        public int Id { get; set; }
+        //public int Id { get; protected set; }
+        public string Nome { get; set; }
+        public string Telefone { get; set; }
+        public bool Ativo  { get; set; } 
+    }
+}
